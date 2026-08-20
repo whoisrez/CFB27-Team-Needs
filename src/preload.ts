786f7f1e769ed3ec7a314b2233e8e6368cdb101f
whoisrez <tymontague19@gmail.com>
2026-08-20 12:25:50 -0400

    Expose dynasty sync action

--- src/preload.ts
+++ src/preload.ts
@@ -2,5 +2,6 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('teamNeedsAPI', {
   chooseAndLoad: () => ipcRenderer.invoke('team-needs:choose-and-load'),
+  prepareSync: () => ipcRenderer.invoke('team-needs:prepare-sync'),
   getTeamBranding: (teamName: string) => ipcRenderer.invoke('team-needs:team-branding', teamName),
 });
