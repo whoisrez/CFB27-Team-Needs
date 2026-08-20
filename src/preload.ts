41e5b4ec60ef5124ac09c38f0d2c6bc99bcfe8e5
whoisrez <tymontague19@gmail.com>
2026-08-20 11:34:27 -0400

    Expose team branding lookup

--- src/preload.ts
+++ src/preload.ts
@@ -2,4 +2,5 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('teamNeedsAPI', {
   chooseAndLoad: () => ipcRenderer.invoke('team-needs:choose-and-load'),
+  getTeamBranding: (teamName: string) => ipcRenderer.invoke('team-needs:team-branding', teamName),
 });
